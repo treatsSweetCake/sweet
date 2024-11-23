f3e1485bdd0b7206648722f77607c1a07a48bdd7
treatsSweetCake <a114919465198@gmail.com>
2024-11-23 18:17:09 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -128,9 +128,8 @@ app.post('/signin', async (req, res) => {
 
 		res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
     sameSite: 'strict',
-    maxAge: 30 * 24 * 60 * 60 * 1000,
 });
 
 		res.status(200).json({ user });

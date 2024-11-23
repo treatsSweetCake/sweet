78439125b05c556f50d945c7d898f12561335e91
treatsSweetCake <a114919465198@gmail.com>
2024-11-23 15:54:56 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ const upload = multer({ storage });
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://sweet-treats-bd.netlify.app', credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());

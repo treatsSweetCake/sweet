c05cfa18dba94af235d07e92c1b9c52459dc53d8
treatsSweetCake <a114919465198@gmail.com>
2024-11-24 23:59:23 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -147,8 +147,8 @@ app.post('/signin', async (req, res) => {
 
 		res.cookie('user_token', token, {
 			httpOnly: true,
-			secure: process.env.NODE_ENV === 'production',
-			sameSite: 'lax',
+			secure: true,
+			sameSite: 'none',
 			maxAge: 30 * 24 * 60 * 60 * 1000,
 		});
 
@@ -177,8 +177,8 @@ app.get('/checkauth', async (req, res) => {
 app.post('/logout', (req, res) => {
 	res.clearCookie('user_token', {
 		httpOnly: true,
-		secure: process.env.NODE_ENV === 'production',
-		sameSite: 'lax',
+		secure: true,
+		sameSite: 'none',
 	});
 	return res.status(200).json({ message: 'Logged out' });
 });
